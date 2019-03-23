a3255c18b4bf72d66fc61c02dda44e13ac6affe0
Josh Nicholas <josh@thambi.li>
2019-03-23 17:02:13 +0530

    Update pocketarchiver.py

--- pocketarchiver.py
+++ pocketarchiver.py
@@ -7,15 +7,21 @@ r = requests.post("https://getpocket.com/v3/get", data = searcher)
 
 r_dict = json.loads(r.text)
 
+# look up individual article ids
+
 items = r_dict['list']
 
 item_list = []
 
+# create a list of article ids to iterate through
+
 for k, v in items.items():
     item_list.append(k)
 
+# delete all the articles corresponding to the articles in the list
+
 for item in item_list:
-    r2 = requests.post('https://getpocket.com/v3/send', data={
+    r2 = requests.post('https://getpocket.com/v3/send', data = {
             'consumer_key': 'CONSUMER KEY',
             'access_token': 'ACCESS TOKEN',
             'actions': json.dumps([{
