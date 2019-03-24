ff4b4c4c2c285014444d6742f9718e1cb8a2d8e4
Josh Nicholas <josh@thambi.li>
2019-03-24 06:57:10 +0530

    Update pocketarchiver.py

--- pocketarchiver.py
+++ pocketarchiver.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-searcher = {"consumer_key":"CONSUMER KEY", "access_token":"ACCESS TOKEN", "count" : "1000", "search":"DOMAIN NAME"}
+searcher = {"consumer_key":"CONSUMER KEY", "access_token":"ACCESS TOKEN", "count" : "1000", "domain":"DOMAIN NAME"}
 
 r = requests.post("https://getpocket.com/v3/get", data = searcher)
 
